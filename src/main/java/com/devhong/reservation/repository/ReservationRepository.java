22c0aa786e127f05c7f83eaf1b429c572cff0296
devhongsa <bippr0901@gmail.com>
2023-06-16 16:51:36 +0900

    feat : [feature/repo] Jpa repository 생성

---
+++ src/main/java/com/devhong/reservation/repository/ReservationRepository.java
@@ -0,0 +1,9 @@
+package com.devhong.reservation.repository;
+
+import com.devhong.reservation.model.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+}
